71c9eba082ab108d5d27e522a79db5993e497483
arcrtic <architdeorukhakar08@gmail.com>
2025-10-30 21:42:39 +0530

    adding name in the main screen to the selected pokemon

--- code/monster_index.py
+++ code/monster_index.py
@@ -105,6 +105,12 @@ class MonsterIndex:
         monster_rect = monster_surf.get_frect(center = top_rect.center)
         self.display_surface.blit(monster_surf, monster_rect)
 
+        # name 
+        name_surf = self.fonts['bold'].render(monster.name, False, COLORS['white'])
+        name_rect = name_surf.get_frect(topleft = top_rect.topleft + vector(10,10))
+        self.display_surface.blit(name_surf, name_rect)
+
+
 
     
     def update(self, dt):
