f5832d25f80dacf258d4c6a06cc25ce8b557341e
arcrtic <architdeorukhakar08@gmail.com>
2025-10-30 22:28:34 +0530

    added get_ stats function

--- code/monster.py
+++ code/monster.py
@@ -20,3 +20,13 @@ class Monster:
     
     def get_stat(self, stat):
         return self.base_stats[stat] * self.level
+    
+    def get_stats(self):
+        return {
+			'health': self.get_stat('max_health'),
+			'energy': self.get_stat('max_energy'),
+			'attack': self.get_stat('attack'),
+			'defense': self.get_stat('defense'),
+			'speed': self.get_stat('speed'),
+			'recovery': self.get_stat('recovery'),
+		}
