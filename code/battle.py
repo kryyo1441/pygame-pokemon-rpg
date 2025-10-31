bd7443604ad693bbe2a107d6a8f1a4d8c1d8771f
arcrtic <architdeorukhakar08@gmail.com>
2025-11-01 00:15:56 +0530

    outline to the option selected

--- code/battle.py
+++ code/battle.py
@@ -90,7 +90,10 @@ class Battle:
 
     def draw_general(self):
        for index, (option, data_dict) in enumerate(BATTLE_CHOICES['full'].items()):
-           surf = self.monster_frame['ui'][data_dict['icon']]
+           if index == self.indexes['general']:
+               surf = self.monster_frame['ui'][f"{data_dict['icon']}_highlight"]
+           else:
+               surf = self.monster_frame['ui'][data_dict['icon']]
            rect = surf.get_rect(center = self.current_monster.rect.midright + data_dict['pos'])
            self.display_surface.blit(surf, rect)
 
