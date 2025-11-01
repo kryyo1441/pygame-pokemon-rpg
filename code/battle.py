3a69626b446766a6f8b198aeae5c678b6c5d8263
vedantgrd <vedantgarud05@gmail.com>
2025-11-01 10:46:50 +0530

    adding indexing to various monsters

--- code/battle.py
+++ code/battle.py
@@ -76,20 +76,25 @@ class Battle:
                     limiter = len(self.current_monster.monster.get_abilities(all = False))
                 case 'switch':
                     limiter = len(self.available_monsters)
+                case 'target':
+                    limiter = len(self.opponent_sprites) if self.selection_side == 'opponent' else len(self.player_sprites)
                 
             if keys[pygame.K_DOWN]:
                 self.indexes[self.selection_mode] = (self.indexes[self.selection_mode] + 1) % limiter
             if keys[pygame.K_UP]:
                 self.indexes[self.selection_mode] = (self.indexes[self.selection_mode] - 1) % limiter
             if keys[pygame.K_SPACE]:
+
+                if self.selection_mode == 'target':
+                    sprite_group = self.opponent_sprites if self.selection_side == 'opponent' else self.player_sprites
+                    sprites = {sprite.pos_index: sprite for sprite in sprite_group}
+                    monster_sprite = sprites[list(sprites.keys())[self.indexes['target']]]
+                    print(monster_sprite.monster)
+
                 if self.selection_mode == 'attacks':
-                    # self.selection_mode = 'target'
-                    self.selected_attack = self.current_monster.monster.get_abilities(all = False)
-                    print(self.selected_attack)
-
-                    #self.selection_side
-
-
+                    self.selection_mode = 'target'
+                    self.selected_attack = self.current_monster.monster.get_abilities(all = False)[self.indexes['attacks']]
+                    self.selection_side = ATTACK_DATA[self.selected_attack]['target']
 
                 if self.selection_mode == 'general':
                     if self.indexes['general'] == 0:
